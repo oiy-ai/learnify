70fdce3e904540fe6e4c0168fcb0724a356ac282
Alex Sun <me@xsun.work>
2025-05-05 17:56:08 +1000

    feat: update MindMapNavigator to support dark mode

--- packages/frontend/core/src/components/learnify/mind-map/navigator/index.tsx
+++ packages/frontend/core/src/components/learnify/mind-map/navigator/index.tsx
@@ -1,10 +1,18 @@
+import { useTheme } from 'next-themes';
+
 import * as styles from './index.css';
 
 export const MindMapNavigator = () => {
+  const { resolvedTheme } = useTheme();
+  const lightImage =
+    'https://cdn.jsdelivr.net/gh/a1exsun/file@main//hexo/20250505154900908.png';
+  const darkImage =
+    'https://cdn.jsdelivr.net/gh/a1exsun/file@main//hexo/20250505173403971.png';
+
   return (
     <div className={styles.root}>
       <img
-        src="https://cdn.jsdelivr.net/gh/a1exsun/file@main//hexo/20250505154900908.png"
+        src={resolvedTheme === 'dark' ? darkImage : lightImage}
         alt="Mind Map Navigator"
         className={styles.image}
       />
