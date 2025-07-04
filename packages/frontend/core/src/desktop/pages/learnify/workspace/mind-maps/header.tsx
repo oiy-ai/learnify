a8472bbc8ce243efcb800c4ece2ee5e53129b236
Alex Sun <me@xsun.work>
2025-07-04 23:44:17 +1000

    feat: duplicate page layout for mind-maps from collections

--- packages/frontend/core/src/desktop/pages/learnify/workspace/mind-maps/header.tsx
+++ packages/frontend/core/src/desktop/pages/learnify/workspace/mind-maps/header.tsx
@@ -1,6 +1,37 @@
+import { FlexWrapper } from '@affine/component';
+import { ExplorerDisplayMenuButton } from '@affine/core/components/explorer/display-menu';
+import { ViewToggle } from '@affine/core/components/explorer/display-menu/view-toggle';
 import { ExplorerNavigation } from '@affine/core/components/explorer/header/navigation';
+import type { ExplorerDisplayPreference } from '@affine/core/components/explorer/types';
 import { Header } from '@affine/core/components/pure/header';
 
-export const MindMapsHeader = () => {
-  return <Header left={<ExplorerNavigation active={'mind-maps'} />} />;
+export const MindMapsHeader = ({
+  displayPreference,
+  onDisplayPreferenceChange,
+}: {
+  displayPreference: ExplorerDisplayPreference;
+  onDisplayPreferenceChange: (
+    // eslint-disable-next-line no-unused-vars
+    displayPreference: ExplorerDisplayPreference
+  ) => void;
+}) => {
+  return (
+    <Header
+      right={
+        <FlexWrapper gap={16}>
+          <ViewToggle
+            view={displayPreference.view ?? 'list'}
+            onViewChange={view => {
+              onDisplayPreferenceChange({ ...displayPreference, view });
+            }}
+          />
+          <ExplorerDisplayMenuButton
+            displayPreference={displayPreference}
+            onDisplayPreferenceChange={onDisplayPreferenceChange}
+          />
+        </FlexWrapper>
+      }
+      left={<ExplorerNavigation active="mind-maps" />}
+    />
+  );
 };
