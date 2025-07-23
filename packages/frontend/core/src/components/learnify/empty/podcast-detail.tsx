ae961c2d126c8139ea5c00e11a573189c444a3ba
Alex Sun <me@xsun.work>
2025-07-23 21:40:48 +1000

    feat: enhance EmptyFlashcardDetail and EmptyPodcastDetail

--- packages/frontend/core/src/components/learnify/empty/podcast-detail.tsx
+++ packages/frontend/core/src/components/learnify/empty/podcast-detail.tsx
@@ -1,13 +1,26 @@
+import { UserFeatureService } from '@affine/core/modules/cloud';
+import type { Collection } from '@affine/core/modules/collection';
+import { WorkspaceDialogService } from '@affine/core/modules/dialogs';
 import { useI18n } from '@affine/i18n';
+import { AllDocsIcon } from '@blocksuite/icons/rc';
+import { useLiveData, useService } from '@toeverything/infra';
+import { useCallback, useEffect } from 'react';
 
+import { ActionButton } from '../../affine/empty/action-button';
+import collectionDetailDark from '../../affine/empty/assets/collection-detail.dark.png';
+import collectionDetailLight from '../../affine/empty/assets/collection-detail.light.png';
 import { EmptyLayout } from '../../affine/empty/layout';
+import { actionGroup } from '../../affine/empty/style.css';
 import type { UniversalEmptyProps } from '../../affine/empty/types';
-import collectionDetailDark from './assets/collection-detail.dark.png';
-import collectionDetailLight from './assets/collection-detail.light.png';
 
-export interface EmptyPodcastDetailProps extends UniversalEmptyProps {}
+export interface EmptyPodcastDetailProps extends UniversalEmptyProps {
+  collection: Collection;
+}
 
-export const EmptyPodcastDetail = (props: EmptyPodcastDetailProps) => {
+export const EmptyPodcastDetail = ({
+  collection,
+  ...props
+}: EmptyPodcastDetailProps) => {
   const t = useI18n();
 
   return (
@@ -16,7 +29,51 @@ export const EmptyPodcastDetail = (props: EmptyPodcastDetailProps) => {
       illustrationDark={collectionDetailDark}
       title={t['com.learnify.empty.podcast-detail.title']()}
       description={t['com.learnify.empty.podcast-detail.description']()}
+      action={
+        BUILD_CONFIG.isMobileEdition ? null : (
+          <Actions collection={collection} />
+        )
+      }
       {...props}
     />
+  );
+};
+
+const Actions = ({ collection }: { collection: Collection }) => {
+  const t = useI18n();
+  const workspaceDialogService = useService(WorkspaceDialogService);
+  const userFeatureService = useService(UserFeatureService);
+  const isAFFiNEAdmin = useLiveData(userFeatureService.userFeature.isAdmin$);
+
+  useEffect(() => {
+    userFeatureService.userFeature.revalidate();
+  }, [userFeatureService]);
+
+  const openAddDocs = useCallback(() => {
+    workspaceDialogService.open('collection-editor', {
+      collectionId: collection.id,
+      mode: 'page',
+    });
+  }, [collection, workspaceDialogService]);
+
+  // const openAddRules = useCallback(() => {
+  //   workspaceDialogService.open('collection-editor', {
+  //     collectionId: collection.id,
+  //     mode: 'rule',
+  //   });
+  // }, [collection, workspaceDialogService]);
+
+  return (
+    <div className={actionGroup}>
+      {isAFFiNEAdmin && (
+        <ActionButton prefix={<AllDocsIcon />} onClick={openAddDocs}>
+          {t['com.affine.empty.collection-detail.action.add-doc']()}
+        </ActionButton>
+      )}
+
+      {/* <ActionButton prefix={<FilterIcon />} onClick={openAddRules}>
+        {t['com.affine.empty.collection-detail.action.add-rule']()}
+      </ActionButton> */}
+    </div>
   );
 };
